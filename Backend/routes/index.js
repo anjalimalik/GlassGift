ad163e94415c3221134d5d9d2d185a6302d439b3
Jordan Pitlor <jordan@jordanpitlor.com>
2018-10-03 23:26:25 -0400

    make functions async

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,31 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../database');
+const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-router.get('/password_reset', function(req, res) {
-    
+router.get('/password_reset', function (req, res) {
+
 });
 
-router.get('api/auth/login', function(req, res) {
+router.get('api/auth/login', async function (req, res) {
 	const user = req.body;
-	const hash = await bcrypt(body.password, saltRounds);
-	var rows = await db.get("GGUser", ["email", "password"], `email = ${user.email} AND password = ${hash}`);
-	if(rows.length == 0){
+	const hash = await bcrypt(user.password, saltRounds);
+	const rows = await db.get("GGUser", ["email", "password"], `email = ${user.email} AND password = ${hash}`);
+	if (rows.length === 0) {
 		throw new Error("User not found");
-	}else{
+	} else {
 		res.send(JSON.stringify(rows));
 	}
 });
 
-router.get('api/updateNGO', function(req, res) {
+router.get('api/updateNGO', async function (req, res) {
 	const changes = req.body;
 	const id_req = `id = ${changes.values.id}`;
-	var rows = await db.get("NGO", ["id"], `id = ${changes.id}`);
-	if(rows.length == 0){
+	let rows = await db.get("NGO", ["id"], `id = ${changes.id}`);
+	if (rows.length === 0) {
 		throw new Error(`No NGO with id ${changes.id} found`);
-	}else{
-		for (var i = 0; i < changes.names.length && i < changes.values.length; i++){
+	} else {
+		for (let i = 0; i < changes.names.length && i < changes.values.length; i++) {
 			await db.modify("NGO", changes.names[i], changes.values[i], id_req);
 		}
 		rows = await db.get("NGO", ["id"], `id = ${changes.id}`);
@@ -33,32 +34,32 @@ router.get('api/updateNGO', function(req, res) {
 	}
 });
 
-router.get('api/updateDonor', function(req, res) {
+router.get('api/updateDonor', async function (req, res) {
 	const changes = req.body;
 	const id_req = `id = ${changes.values.id}`;
-	var rows = await db.get("Donor", ["id"], `id = ${changes.id}`);
-	if(rows.length == 0){
+	const rows = await db.get("Donor", ["id"], `id = ${changes.id}`);
+	if (rows.length === 0) {
 		throw new Error(`No Donor with id ${changes.id} found`);
-	}else{
-		for (var i = 0; i < changes.names.length && i < changes.values.length; i++){
-			db.modify("Donor", changes.names[i], changes.values[i], id_req);
+	} else {
+		for (let i = 0; i < changes.names.length && i < changes.values.length; i++) {
+			await db.modify("Donor", changes.names[i], changes.values[i], id_req);
 		}
 	}
 });
 
-router.get('api/getNGONotice', function(req, res) {
+router.get('api/getNGONotice', async function (req, res) {
 	const NGOuser = req.body;
-	var rows = await db.get("NGO", ["id"], `id = ${NGOuser.id}`);
-	if(rows.length == 0){
+	const rows = await db.get("NGO", ["id"], `id = ${NGOuser.id}`);
+	if (rows.length === 0) {
 		throw new Error(`NGO with id ${NGOuser.id} not found`);
-	}else if(rows.length > 1){
+	} else if (rows.length > 1) {
 		throw new Error(`Several NGOs with id ${NGOuser.id} found`);
 	} else {
 		res.send(JSON.stringify(rows));
 	}
 });
 
-router.get('api/searchNGO', function(req, res){
+router.get('api/searchNGO', function (req, res) {
 	//To be discovered
 });
 
