1f54745c807a5dc7fd211b9af48b1c5e0da0928e
Noah Rinehart <rinehart.noah@gmail.com>
2018-10-05 00:49:36 -0400

    rewriting backend routes in progress

---
+++ Backend/database/drop.sql
@@ -0,0 +1,17 @@
+drop table if exists GGUser cascade;
+
+drop table if exists NGO cascade;
+
+drop table if exists NGOCategories cascade;
+
+drop table if exists Donor cascade;
+
+drop table if exists Searches cascade;
+
+drop table if exists Subscriptions cascade;
+
+drop table if exists Donation cascade;
+
+drop table if exists RecurringDonation cascade;
+
+drop table if exists UserIps cascade;
