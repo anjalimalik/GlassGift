12424638631b7465873f5fa04bffda2a2e691b54
Peter Xeros <pxeros@purdue.edu>
2018-10-05 00:56:26 -0400

    Email Functions fixed and ready to go
    Yee hee hee

--- Backend/email/index.js
+++ Backend/email/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const mailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
 	service: 'gmail',
 	auth: {
-		user: 'glassgiftteam@gmail.com'
+		user: 'glassgiftteam@gmail.com',
 		pass: 'jack3dj3sus'
 	}
 });
@@ -16,25 +16,26 @@ async function sendConfirmationEmail(email, name, confirmationLink, type) {
 			   `Best Regards,\nThe GlassGift Team\n\n`+
 			   `P.S.: Please don't respond to this email, we won\'t see it.\n`; 
 
+
 	var mailoptions = {
-		from: 'glassgiftteam@gmail.com'
+		from: 'glassgiftteam@gmail.com',
 		to: email,
 		subject: 'Confirmation Email: Please Confirm Your Account',
 		text: body
 	};
 
 	transporter.sendMail(mailoptions, function(err, info){
-		if(error){
-			console.log(error);
+		if(err){
+			return console.error(err);
 		}else{
-			console.log('Email sent' + info.response);
+			return console.log('Email sent' + info.response);
 		}
 	});
 }
 
 async function sendIPemail(email, ipAddr){
 	var body = `Dear User,\n\nHello from GlassGift! We noticed that you had logged in to your` +
-		`account from a different IP adress than normal, the IP address you logged in from is:\n\n\t${ipAddr}` +
+		` account from a different IP adress than normal, the IP address you logged in from is:\n\n\t${ipAddr}` +
 		`\n\nIf this was you, then you can disregard this email. Otherwise, we highly reccomend that you reset your` +
 		`password as soon as possible. You can do so by going to the following URL.\n\n\t` +
 		`URL\n\n` +
@@ -43,19 +44,44 @@ async function sendIPemail(email, ipAddr){
 		`P.S.: Please don't respond to this email, we won\'t see it.\n`; 
 
 	var mailoptions = {
-		from: 'glassgiftteam@gmail.com'
+		from: 'glassgiftteam@gmail.com',
 		to: email,
 		subject: 'GlassGift: Login from unusual IP Address',
 		text: body
 	};
 
 	transporter.sendMail(mailoptions, function(err, info){
-		if(error){
-			console.log(error);
+		if(err){
+			return console.error(err);
 		}else{
-			console.log('Email sent' + info.response);
+			return console.log('Email sent' + info.response);
 		}
 	});
 }
 
-module.exports = {sendIPemail, sendConfirmationEmail}
+async function sendForgotPasswordemail(email, token){
+	var body = `Dear User,\n\nHello from GlassGift! We noticed that you wish to reset or have forgotten`+
+		`your password, this is not issue, however we do ask that you follow this link:\n\n`+
+		`URL/token=${token}\n\nThis is sipmly so we can verify that you are who you say you are.\n`+
+		`You will have 24 hours to reset your password, after that, the link we gave you will expire.\n`+
+		`Thank you for using GlassGift! Hopefully we will never encounter this issue again.\n\n`+
+		`Best Regards,\nThe GlassGift Team\n\n`+
+		`P.S.: Please don't respond to this email, we won\'t see it.\n`; 
+
+	var mailoptions = {
+		from: 'glassgiftteam@gmail.com',
+		to: email,
+		subject: 'GlassGift: Reset your password!',
+		text: body
+	};
+
+	transporter.sendMail(mailoptions, function(err, info){
+		if(err){
+			return console.error(err);
+		}else{
+			return console.log('Email sent' + info.response);
+		}
+	});
+}
+
+module.exports = {sendIPemail, sendConfirmationEmail, sendForgotPasswordemail}
