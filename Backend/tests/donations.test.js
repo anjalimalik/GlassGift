32c073af5e48a8eacef61a76a462c71119719cc7
Jordan Pitlor <jordan@jordanpitlor.com>
2018-10-23 20:43:17 -0400

    add final skeletons

---
+++ Backend/tests/donations.test.js
@@ -0,0 +1,9 @@
+describe("Donations", () => {
+	it("A donation outside a minimum/maximum limit of an NGO should fail", () => {
+
+	});
+
+	it("Viewing anonymous payments shouldn't reveal the donor's identity", () => {
+
+	});
+});
--- Backend/tests/donor.test.js
+++ Backend/tests/donor.test.js
@@ -1,17 +1,17 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const server = require('../app')
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
 
-let should = chai.should()
+let should = chai.should();
 let expect = chai.expect;
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
 describe('Testing registering donor account', () => {
   let donor = {
     email: 'test@gmail.com',
     password: 'test',
 	  location: "test location",
-  }
+  };
 
   it('POST - Test donor registration', (done) => {
     chai.request(server)
@@ -19,12 +19,12 @@ describe('Testing registering donor account', () => {
       .set('content-type', 'application/json')
       .send(donor)
       .end((err, res) => {
-        if (err) console.error('Error: donor registration failed')
-        res.should.have.status(200) // 500 for error
+        if (err) console.error('Error: donor registration failed');
+        res.should.have.status(200); // 500 for error
         expect(err).to.be.null;
         done()
       })
-  })
+  });
 
   it('POST - Test duplicate donor registration', (done) => {
     chai.request(server)
@@ -32,10 +32,10 @@ describe('Testing registering donor account', () => {
       .set('content-type', 'application/json')
       .send(donor)
       .end((err, res) => {
-        res.body.should.have.property('error').that.equals('Already exists')
+        res.body.should.have.property('error').that.equals('Already exists');
         done()
       })
-  })
+  });
 
   it('POST - Test donor login', (done) => {
     chai.request(server)
@@ -46,16 +46,18 @@ describe('Testing registering donor account', () => {
         password: donor.password
       })
       .end((err, res) => {
-        if (err) console.error('Error: donor login failed')
-        res.should.have.status(200)
-        res.body.should.be.a('object')
-        res.body.should.have.property('token')
-        res.body.should.have.property('user')
+        if (err) console.error('Error: donor login failed');
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('token');
+        res.body.should.have.property('user');
         expect(err).to.be.null;
 
-        donor.id = res.body.user.id
-        donor.token = res.body.token
+        donor.id = res.body.user.id;
+        donor.token = res.body.token;
         done()
-      })
-  })
-})
+      });
+  });
+
+  it('Adding payment methods stores them for the future')
+});
