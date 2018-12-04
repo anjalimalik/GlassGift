17af2befdcc7c6cf55c310eb564acbd95010628e
Peter Xeros <pxeros@purdue.edu>
2018-12-04 17:11:45 -0500

    Fixed issue with calendar
    I fixed it.

--- Backend/routes/ngo.js
+++ Backend/routes/ngo.js
@@ -128,8 +128,8 @@ router.post('/visualCalendar', async function(req, res){
 
 	return res.status(200).json({
 		numDonations: numDonations[0].count,
-		totalMoney: `\$${parseInt(totalMoney[0].sum | 0)/100}.${parseInt(totalMoney[0].sum | 0)%100 < 10? '0' + parseInt(totalMoney[0].sum | 0)%100: parseInt(totalMoney[0].sum | 0)%100} `,
-		averageDonation: `\$${parseInt(averageDonation[0].avg | 0)/100}.${parseInt(averageDonation[0].avg | 0)%100 < 10 ? '0' + parseInt(averageDonation[0].avg | 0)%100: parseInt(averageDonation[0].avg | 0)%100}`,
+		totalMoney: `\$${Math.floor((totalMoney[0].sum | 0)/100)}.${Math.floor((totalMoney[0].sum | 0)%100) < 10? '0' + Math.floor((totalMoney[0].sum | 0)%100): Math.floor((totalMoney[0].sum | 0)%100)}`,
+		averageDonation: `\$${Math.floor((averageDonation[0].avg | 0)/100)}.${Math.floor((averageDonation[0].avg | 0)%100) < 10 ? '0' + Math.floor((averageDonation[0].avg | 0)%100): Math.floor((averageDonation[0].avg | 0)%100)}`,
 		averageAge: averageAge,
 	});
 });
