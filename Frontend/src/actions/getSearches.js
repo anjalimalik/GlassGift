d1e8370e174b7481bf7ad4997eea16b037b8e29e
Noah Rinehart <rinehart.noah@gmail.com>
2018-12-05 23:46:04 -0500

    frontend search history changes

--- Frontend/src/actions/getSearches.js
+++ Frontend/src/actions/getSearches.js
@@ -37,39 +37,39 @@ function callGetSearchesApi() {
   return new Promise((resolve, reject) => {
     const token = getUserToken();
     if (!token) reject(new Error("No token!"));
-    axios.post('http://localhost:3000/getSearches', { headers: { Authorization: token }})
+    axios.get('http://localhost:3000/searchHistory', { headers: { Authorization: token }})
     .then(response => resolve(response.data))
     .catch(error => reject(new Error(error.response.data.error)));
   })
 }
 
-// export function getSearches() {
-//   const request = callGetSearchesApi(email, password);
-//   return dispatch => {
-//     dispatch(getSearchesPending(true));
-//     return request
-//     .then(response => {
-//       dispatch(getSearchesSuccess(response));
-//     })
-//     .catch(error => dispatch(getSearchesError(error)));
-//   };
-// }
-
-const response = [
-  {
-    text: 'Test',
-  },
-  {
-    text: 'Test2',
-  },
-  {
-    text: 'Test1',
-  },
-];
-
 export function getSearches() {
+  const request = callGetSearchesApi();
   return dispatch => {
     dispatch(getSearchesPending(true));
-    setTimeout(() => dispatch(getSearchesSuccess(response)), 500);
-  }
+    return request
+    .then(response => {
+      dispatch(getSearchesSuccess(response));
+    })
+    .catch(error => dispatch(getSearchesError(error)));
+  };
 }
+
+// const response = [
+//   {
+//     text: 'Test',
+//   },
+//   {
+//     text: 'Test2',
+//   },
+//   {
+//     text: 'Test1',
+//   },
+// ];
+
+// export function getSearches() {
+//   return dispatch => {
+//     dispatch(getSearchesPending(true));
+//     setTimeout(() => dispatch(getSearchesSuccess(response)), 500);
+//   }
+// }
